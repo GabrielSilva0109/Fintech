badb58101cdd78e7bd106c649937f333d0efade5
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-01-09 13:42:53 -0300

    1

--- src/Components/MesBtn.tsx
+++ src/Components/MesBtn.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useData } from "../Context/DataContext";
 
 
 const style: React.CSSProperties= {
@@ -18,20 +19,31 @@ function nomeMes(n:number){
     
 }
 
+function formatDate(date: Date) {
+    const dd = String(date.getDate()).padStart(2, "0")
+    const mm = String(date.getMonth() + 1).padStart(2, "0")
+    const yyyy = String(date.getFullYear())
+    return `${yyyy}-${mm}-${dd}`
+}
+
 
 const MesBtn = ({ n }: {n: number}) =>{
+    const {setInicio, setFinal} = useData()
+
     function setMes(n:number){
         const date = new Date()
         date.setMonth(date.getMonth() + n)
 
         const firstDay = new Date(date.getFullYear(), date.getMonth(), 1)
-        const LastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0)
-        console.log(LastDay)
+        const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0)
+
+        setInicio(formatDate(firstDay))
+        setFinal(formatDate(lastDay))
+        
     }
 
-    setMes(n)
 
-    return <button style={style}>{nomeMes(n)}</button>
+    return <button style={style} onClick={() => setMes(n)}>{nomeMes(n)}</button>
 }
 
 export default MesBtn
